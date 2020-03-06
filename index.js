2fbb817da691ae879debaf2d5d8a45b0b4204238
crisp-language-1475 <malihsundus@gmail.com>
2020-03-06 16:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space."; 
 =======
 var topic = "space"; 
->>>>>>> 5741da0519f20015d525caafa9322e3ab8c74bda
-
 var conversation ="Let\'s talk about "+ `${topic}`;
