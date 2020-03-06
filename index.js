3fedcc49ec68b3cda255c382dd346575c4180841
crisp-language-1475 <malihsundus@gmail.com>
2020-03-06 16:06:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
+<<<<<<< HEAD
 var topic = "space."; 
+=======
+var topic = "space"; 
+>>>>>>> 5741da0519f20015d525caafa9322e3ab8c74bda
 
 var conversation ="Let\'s talk about "+ `${topic}`;
